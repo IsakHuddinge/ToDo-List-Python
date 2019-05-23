9cb88fcc4b10860796239c3c3bc94a5f0b0ca73e
IsakHuddinge <isak.nywall@edu.huddinge.se>
2019-05-23 11:58:36 +0200

    add clear screen lambda function

--- todo.py
+++ todo.py
@@ -1,25 +1,32 @@
+import os
+# Clear console.
+# cls for windows, clear for linux.
+clear = lambda: os.system('cls' if os.name == "nt" else 'clear')
+
+
 def displayList(myList):
     if len(myList) > 0:
-        print("Your List: ")
+        print("Your List:")
         # Go through all items in the list.
         for i in range(len(myList)):
             # Print index + 1 for readabiltiy.
             # No new line at end.
             print("[", i + 1, "] ", end="")
             
-            print(myList[i]["title"],":")
+            print(myList[i]["title"] + ":")
             print(myList[i]["description"])
             # New Line for readability.
             print("\nDeadline:", myList[i]["deadline"])
     else:
         print("Your List is empty.")
 
-#creates a new dictionary and adds it to a list.
+# Creates a new dictionary and adds it to a list.
 def addToList(myList, title, description, deadline):
     myDictionary = {
-        "title": title,
-        "description": description,
-        "deadline": deadline
+        "title": title.strip(),
+        "description": description.strip(),
+        "deadline": deadline.strip(),
+        "done": False
     }
     myList.append(myDictionary)
     print("Added item to list.")
@@ -37,12 +44,14 @@ def newTaskInput():
 toDoList = []
 # loop for repeated user input.
 while True:
+    # Clear console and display list
+    clear()
     displayList(toDoList)
-    userInput = input(">")
+    userInput = input(">").lower().strip()
     # Create a new todo
-    if userInput.lower() == "n":
+    if userInput == "n":
         title, description, deadline = newTaskInput()
         addToList(toDoList, title, description, deadline)
     # Break out of loop, ending program
-    elif userInput.lower() == "q":
+    elif userInput == "q":
         break
